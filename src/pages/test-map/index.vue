f5fc185603a535c35235bbb8453a8bd878742db8
licunxing <864255598@qq.com>
2025-10-26 23:22:40 +0800

    feat: 自定义当个区域图片

---
+++ src/pages/test-map/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <view class="content">
+    <view class="text-area">
+      <text class="title">测试地图</text>
+      <custom-map name="uni-app"></custom-map>
+    </view>
+  </view>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+const title = ref('Hello')
+</script>
+
+<style>
+.container {
+  width: 100vw;
+  height: 60vh;
+}
+
+</style>
---
+++ src/wxcomponents/map/index.js
@@ -0,0 +1,128 @@
+import * as echarts from "../ec-canvas/echarts.js";
+import geoJson from "./mapData.js";
+
+const app = getApp();
+
+function initChart(canvas, width, height, dpr) {
+  const chart = echarts.init(canvas, null, {
+    width: width,
+    height: height,
+    devicePixelRatio: dpr, // new
+  });
+  canvas.setChart(chart);
+
+  echarts.registerMap("henan", geoJson);
+
+  const option = {
+    tooltip: {
+      trigger: "item",
+      formatter: "{b}: {c}",
+    },
+
+    toolbox: {
+      show: true,
+      orient: "vertical",
+      left: "right",
+      top: "center",
+    },
+    series: [
+      {
+        type: "map",
+        mapType: "henan",
+        label: {
+          normal: {
+            show: true,
+          },
+          emphasis: {
+            textStyle: {
+              color: "#fff",
+            },
+          },
+        },
+        itemStyle: {
+          normal: {
+            borderColor: "#389BB7",
+            areaColor: "#389BB7",
+          },
+          emphasis: {
+            borderColor: "#389BB7",
+            areaColor: "#389BB7",
+          },
+        },
+        data: [
+          {
+            name: "郑州市",
+            itemStyle: {
+              normal: {
+                borderColor: "#389BB7",
+                areaColor: "#389BB7",
+              },
+              emphasis: {
+                areaColor: {
+                  image:
+                    "/static/pngtree-floating-realistic-clouds-png-image_8623463.png", // 支持为 HTMLImageElement, HTMLCanvasElement，不支持路径字符串
+                  repeat: "repeat", // 是否平铺，可以是 'repeat-x', 'repeat-y', 'no-repeat'
+                },
+                borderWidth: 0,
+              },
+            },
+          },
+          {
+            name: "洛阳市",
+            value: 10,
+            itemStyle: {
+              normal: {
+                borderColor: "#000000",
+                areaColor: {
+                  image:
+                    "/static/pngtree-floating-realistic-clouds-png-image_8623463.png",
+                  repeat: "repeat",
+                },
+              },
+              emphasis: {
+                areaColor: {
+                  image:
+                    "/static/pngtree-floating-realistic-clouds-png-image_8623463.png", // 支持为 HTMLImageElement, HTMLCanvasElement，不支持路径字符串
+                  repeat: "repeat", // 是否平铺，可以是 'repeat-x', 'repeat-y', 'no-repeat'
+                },
+                borderWidth: 0,
+              },
+            },
+          },
+          { name: "开封市", value: 20 },
+          { name: "信阳市", value: 30 },
+          { name: "驻马店市", value: 40 },
+          { name: "南阳市", value: 41 },
+          { name: "周口市", value: 15 },
+          { name: "许昌市", value: 25 },
+          { name: "平顶山市", value: 35 },
+          { name: "新乡市", value: 35 },
+          { name: "漯河市", value: 35 },
+          { name: "商丘市", value: 35 },
+          { name: "三门峡市", value: 35 },
+          { name: "济源市", value: 35 },
+          { name: "焦作市", value: 35 },
+          { name: "安阳市", value: 35 },
+          { name: "鹤壁市", value: 35 },
+          { name: "濮阳市", value: 35 },
+          { name: "开封市", value: 45 },
+        ],
+        animation: false,
+      },
+    ],
+  };
+
+  chart.setOption(option);
+
+  return chart;
+}
+
+Page({
+  data: {
+    ec: {
+      onInit: initChart,
+    },
+  },
+
+  onReady() {},
+});
